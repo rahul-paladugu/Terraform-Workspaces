58705b17c5a792b611eee52fa138572095550953
rahul paladugu <rahulpaladugu96@gmail.com>
2025-11-07 15:46:06 +1300

    workspace

--- locals.tf
+++ locals.tf
@@ -8,5 +8,5 @@ locals {
   ec2_tags = merge(local.common_tags, { Name = "workspace-${local.environment}-${var.project}" })
   sg_tags = merge(local.common_tags, { Name = "sg-${local.environment}-${var.project}" })
   dns_record = "workspace-${local.environment}-${var.project}"
-  sg_name = "sg-${local.environment}-${var.project}" 
+  sg_name = "${local.environment}-${var.project}-sg"
 }
