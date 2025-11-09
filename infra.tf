05cacd8e9e11e0fac75651fe99ba1e35bf85dfd3
rahul paladugu <rahulpaladugu96@gmail.com>
2025-11-09 14:21:36 +1300

    T

--- infra.tf
+++ infra.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "roboshop" {
-  ami = data.aws_ami.rahul-practice.id
-  instance_type = lookup(var.instance_type, terraform.workspace)
-  vpc_security_group_ids = [aws_security_group.allow-network.id]
-  tags = local.ec2_tags
+  ami = data.aws_ami.rahul-practice.id #used the data module to find the ami_id.
+  instance_type = lookup(var.instance_type, terraform.workspace) #Lookup function used to find the key value of environment
+  vpc_security_group_ids = [aws_security_group.allow-network.id] #picked from the SG created below
+  tags = local.ec2_tags #Attached the tags hardcoded in locals.tf
 }
 
 resource "aws_security_group" "allow-network" {
     name = local.sg_name
-    dynamic "egress" {
+    dynamic "egress" {  #Dynamic block is used to iterate the block of content and updated the port numbers.
       for_each = toset(var.network_ports)
       content {
         from_port = egress.value
@@ -16,7 +16,7 @@ resource "aws_security_group" "allow-network" {
         cidr_blocks = ["0.0.0.0/0"]
       }
     }
-    dynamic "ingress" {
+    dynamic "ingress" { #Dynamic block is used to iterate the block of content and updated the port numbers.
       for_each = toset(var.network_ports)
       content {
         from_port = ingress.value
@@ -29,7 +29,7 @@ resource "aws_security_group" "allow-network" {
 }
 
 resource "aws_route53_record" "dns-records" {
-    zone_id = data.aws_route53_zone.roboshop.zone_id
+    zone_id = data.aws_route53_zone.roboshop.zone_id #used the data module to find the zone id.
     name = local.dns_record
     type = "A"
     ttl = 1
